26ad438fc890ae2689fd8163e32a730a093de907
Abdallah_Abdelsabour <abdallah97158@gmail.com>
2022-11-09 18:49:33 +0200

    unique number orderd list

---
+++ unique_number_ordered.py
@@ -0,0 +1,22 @@
+
+
+
+
+def unique_number_orderd(lst):
+
+  unique_list = []
+  temp = None
+  for item in lst :
+    if item !=temp :
+      unique_list.append(item)
+      temp = item
+
+  return unique_list  
+
+
+
+result = unique_number_orderd([1,1,1,2,2,4,4,5,5,1])
+
+print(result)
+
+
